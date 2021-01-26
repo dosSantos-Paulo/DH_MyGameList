9320106ae21394e7ff3cb00a97e9ada674765e02
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-26 14:28:23 -0300

    criando tela de splashLogin

---
+++ app/src/main/java/com/devdossantos/mygamelist/view/SplashLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.devdossantos.mygamelist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devdossantos.mygamelist.R
+
+class SplashLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_login)
+    }
+}
