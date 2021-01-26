2a154aca90fbbcc7f5d33ddbab25e4229f36303a
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-26 14:43:45 -0300

    splash animation

--- app/src/main/java/com/devdossantos/mygamelist/view/SplashLoginActivity.kt
+++ app/src/main/java/com/devdossantos/mygamelist/view/SplashLoginActivity.kt
@@ -2,11 +2,34 @@ package com.devdossantos.mygamelist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
 import com.devdossantos.mygamelist.R
+import com.google.android.material.card.MaterialCardView
 
 class SplashLoginActivity : AppCompatActivity() {
+
+    private val _background by lazy { findViewById<ImageView>(R.id.background_splash) }
+
+    private val _cardView by lazy { findViewById<MaterialCardView>(R.id.cardView_splashLogin) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_login)
+
+        _background.animate().apply {
+            alpha(1f)
+            duration = 1500
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            _cardView.animate().apply {
+                alpha(1f)
+                duration = 1000
+            }
+        }, 3000)
+
+
     }
 }
