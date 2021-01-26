b008309b9913dfd09ad7dd370d2b2eda7fd31566
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-26 15:19:16 -0300

    criando validação dos campos de login

--- app/src/main/java/com/devdossantos/mygamelist/view/RegisterActivity.kt
+++ app/src/main/java/com/devdossantos/mygamelist/view/RegisterActivity.kt
@@ -1,12 +1,57 @@
 package com.devdossantos.mygamelist.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devdossantos.mygamelist.MainActivity
 import com.devdossantos.mygamelist.R
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val _createAcctountButton by lazy { findViewById<MaterialButton>(R.id.button_createAccount_register) }
+
+    private val _name by lazy { findViewById<TextInputLayout>(R.id.textFile_name_register) }
+
+    private val _email by lazy { findViewById<TextInputLayout>(R.id.textField_email_register) }
+
+    private val _password by lazy { findViewById<TextInputLayout>(R.id.textField_password_register) }
+
+    private val _repeatePassword by lazy { findViewById<TextInputLayout>(R.id.textField_repeatPassword_register) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        _createAcctountButton.setOnClickListener {
+
+            if (_name.editText?.text.isNullOrEmpty()) {
+                _name.editText?.error = getString(R.string.name_error)
+            }
+
+            if (_email.editText?.text.isNullOrEmpty()) {
+                _email.editText?.error = getString(R.string.email_error)
+            }
+
+            if (_password.editText?.text.isNullOrEmpty()) {
+                _password.editText?.error = getString(R.string.password_error)
+            }
+
+            if (_repeatePassword.editText?.text?.trim().toString() != _password.editText?.text?.trim().toString()) {
+                _repeatePassword.editText?.error = getString(R.string.repeat_password_error)
+            }
+
+            if (!_name.editText?.text.isNullOrEmpty() &&
+                !_email.editText?.text.isNullOrEmpty() &&
+                !_password.editText?.text.isNullOrEmpty() &&
+                _repeatePassword.editText?.text?.trim().toString() == _password.editText?.text?.trim().toString()
+            ) {
+                intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/devdossantos/mygamelist/view/SplashLoginActivity.kt
+++ app/src/main/java/com/devdossantos/mygamelist/view/SplashLoginActivity.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.ImageView
+import com.devdossantos.mygamelist.MainActivity
 import com.devdossantos.mygamelist.R
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class SplashLoginActivity : AppCompatActivity() {
 
@@ -18,11 +21,34 @@ class SplashLoginActivity : AppCompatActivity() {
 
     private val _createAcountButton by lazy { findViewById<MaterialButton>(R.id.button_createAccount_splashLogin) }
 
+    private val _loginButton by lazy { findViewById<MaterialButton>(R.id.button_login_splashLogin) }
+
+    private val _email by lazy { findViewById<TextInputLayout>(R.id.textFile_email_splashLogin) }
+
+    private val _password by lazy { findViewById<TextInputLayout>(R.id.textField_password_splashLogin) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_login)
 
         splashScreenAnimation()
+
+        _loginButton.setOnClickListener {
+
+            if (_email.editText?.text.isNullOrEmpty()) {
+                _email.editText?.error = getString(R.string.email_error)
+            }
+
+            if (_password.editText?.text.isNullOrEmpty()) {
+                _password.editText?.error = getString(R.string.password_error)
+            }
+
+            if (!_email.editText?.text.isNullOrEmpty() && !_password.editText?.text.isNullOrEmpty()) {
+                intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
 
         _createAcountButton.setOnClickListener {
             intent = Intent(this, RegisterActivity::class.java)
