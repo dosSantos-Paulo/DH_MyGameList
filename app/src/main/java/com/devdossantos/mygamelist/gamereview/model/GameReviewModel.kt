0091e4553238d79d78b2bf20893184442e6bfbaa
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-26 15:26:56 -0300

    criando classe gameReview

---
+++ app/src/main/java/com/devdossantos/mygamelist/gamereview/model/GameReviewModel.kt
@@ -0,0 +1,8 @@
+package com.devdossantos.mygamelist.gamereview.model
+
+data class GameReviewModel (
+    val imageUri: String,
+    val name: String,
+    val createdAt: Int,
+    val description: String
+)
